57a132b2c0c4245b6c391826e2383f690cbbe6b1
Grateo Alfando <107740132+Alfando345@users.noreply.github.com>
2023-08-16 10:43:40 +0700

    Add files via upload

---
+++ pydown.py
@@ -0,0 +1,15 @@
+import requests
+
+def download_file(url, save_path):
+    response = requests.get(url)
+    file_name = url.split("/")[-1]  # Mengambil nama file dari URL
+    file_path = save_path + "/" + file_name
+
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+    print("File berhasil diunduh dan disimpan di:", file_path)
+
+url = input("Masukkan URL file yang ingin diunduh: ")
+save_path = "/sdcard/download"
+
+download_file(url, save_path)
